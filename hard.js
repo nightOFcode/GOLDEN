5f5e6783af273cc49993572f98bb02c2baf726e3
Amir Hossein Shirazi <a.h.shirazi1380.10@gmail.com>
2021-11-30 21:52:54 +0330

    first commit

---
+++ hard.js
@@ -0,0 +1,46 @@
+var dis = $("#sidebar").is(':visible')
+// alert(dis);
+
+$(document).ready(function () {
+    $('#sidebar-button').click(function () {
+        if ($('#sidebar').css('display') == 'none') {
+            // $('#sidebar').css('display', 'block');
+            $('#sidebar').slideToggle(1000);
+            $('#sidebar-button').css('left', '230px');
+        } else {
+            // $('#sidebar').css('display', 'none');
+            $('#sidebar').slideToggle(500);
+            $('#sidebar-button').css('left', '10px');
+        }
+
+        if (dis == 'false') {
+            $('#categories').show();
+        } else {
+            $('#categories').hide();
+        }
+    });
+
+    $('#category-btn').click(function () {
+        if ($('#categories').css('display') == 'none') {
+            $('#categories').slideToggle(1000);
+        } else
+            $('#categories').slideToggle(1000);
+    });
+});
+
+var timeoutID;
+
+$(document).ready(function () {
+    // start hiding the message after 2 seconds
+    timeoutID = window.setTimeout(hideMessage, 2000);
+});
+
+function hideMessage() {
+    $('#loading-holder').animate({
+        opacity: 0
+    }, 800, function () {
+        $('#loading-holder ').css({
+            display: 'none'
+        });
+    });
+}
