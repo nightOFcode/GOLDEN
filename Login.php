4ba772a2789a90cc6ec14739c9c08db10ecf42a5
Amir Hossein Shirazi <a.h.shirazi1380.10@gmail.com>
2021-12-05 14:15:25 +0330

    fixing css problems and ...

--- Login.php
+++ Login.php
@@ -1,56 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <?php
- include 'inc/header.php'
+include 'inc/header.php';
 ?>
+
 <body>
+
 <?php
-include 'inc/navbar.php'
+include 'inc/navbar.php';
+include 'inc/sidebar.php';
 ?>
-<nav id="sidebar">
-    <a href="index.php" class="holder">
-        <i class='bx bxs-home' id="home-icon"></i>
-        Home
-    </a>
-    <a href="Contact.php" class="holder">
-        <i class='bx bxs-detail'></i>
-        Contact
-    </a>
-    <div class="holder" id="category-btn">
-        <i class='bx bxs-category'></i>
-        Categories
-    </div>
-    <div id="categories">
-        <a href="Cars.php" class="list">
-            <i class='bx bxs-car'></i>
-            Cars
-        </a>
-        <a href="Nature.php" class="list">
-            <i class='bx bxs-tree'></i>
-            Nature
-        </a>
-        <a href="Sea.php" class="list">
-            <i class='bx bx-water'></i>
-            Sea
-        </a>
-        <a href="Neon.php" class="list">
-            <i class='bx bx-meh-blank'></i>
-            Neon
-        </a>
-        <a href="Text.php" class="list">
-            <i class='bx bx-text'></i>
-            Text
-        </a>
-        <a href="Trucks.php" class="list">
-            <i class='bx bxs-truck'></i>
-            Trucks
-        </a>
-    </div>
-    <a href="Login.php" class="holder">
-        <i class='bx bx-log-in'></i>
-        Login
-    </a>
-</nav>
+
 <div id="login-holder">
     <h1>
         Login
---
+++ inc/sidebar.php
@@ -0,0 +1,48 @@
+<?php
+include 'inc/style.php'
+?>
+
+<nav id="sidebar">
+    <a href="index.php" class="holder">
+        <i class='bx bxs-home' id="home-icon"></i>
+        Home
+    </a>
+    <a href="Contact.php" class="holder">
+        <i class='bx bxs-detail'></i>
+        Contact
+    </a>
+    <div class="holder" id="category-btn">
+        <i class='bx bxs-category'></i>
+        Categories
+    </div>
+    <div id="categories">
+        <a href="Cars.php" class="list">
+            <i class='bx bxs-car'></i>
+            Cars
+        </a>
+        <a href="Nature.php" class="list">
+            <i class='bx bxs-tree'></i>
+            Nature
+        </a>
+        <a href="Sea.php" class="list">
+            <i class='bx bx-water'></i>
+            Sea
+        </a>
+        <a href="Neon.php" class="list">
+            <i class='bx bx-meh-blank'></i>
+            Neon
+        </a>
+        <a href="Text.php" class="list">
+            <i class='bx bx-text'></i>
+            Text
+        </a>
+        <a href="Trucks.php" class="list">
+            <i class='bx bxs-truck'></i>
+            Trucks
+        </a>
+    </div>
+    <a href="Login.php" class="holder">
+        <i class='bx bx-log-in'></i>
+        Login
+    </a>
+</nav>
